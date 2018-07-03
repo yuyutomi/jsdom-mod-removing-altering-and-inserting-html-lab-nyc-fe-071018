20e2aa856fd0d2119f857a264bc317754a74581b
Steven G. Harms <steven.harms@flatironschool.com>
2018-07-03 11:59:34 -0400

    Add solution

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-/* Enter the code to remove the main node element under this comment */
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement("h1");
+newHeader.innerText = "Steven is the champion!";
+newHeader.className = "victory";
