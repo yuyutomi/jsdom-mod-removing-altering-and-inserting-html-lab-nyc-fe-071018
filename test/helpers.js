1802719b61ca21b60879785d15d0be0953517a3c
maxwell <github email address>
2018-07-03 10:27:39 -0400

    udpated helpers.js, switched to chai

--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,20 @@
-global.expect = require('expect')
-
+const chai = require('chai')
+global.expect = chai.expect
 const fs = require('file-system')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
+const babel = require('babel-core');
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
-jsdom({ html })
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'index.js'), {
+    presets: ['env']
+  }
+);
+
+const src = babelResult.code
+
+jsdom({
+  html, src
+});
--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@
 
 describe("index.html", () => {
   it("exists", () => {
-    expect(document.querySelector('main')).toExist('add a <main> element to index.html')
-    expect(document.querySelector('#test')).toExist('add an element with id "test"')
+    expect(document.querySelector('main')).to.exist
+    expect(testVar).to.equal('hi')
   })
 })
