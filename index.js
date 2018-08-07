21d0096594d1dfa6a5cb52272d1aa574e0f3aadb
maxwell <github email address>
2018-08-07 10:57:29 -0400

    updated solution

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
+newHeader.id = "victory"
 newHeader.innerText = "Steven is the champion!";
 newHeader.className = "victory";
