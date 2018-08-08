4ca64e81d1858d7ec9c44ad51688ee853f8a90ef
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-08 00:57:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+document.getElementById('main').remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerText = 'yuri is the champion!'
