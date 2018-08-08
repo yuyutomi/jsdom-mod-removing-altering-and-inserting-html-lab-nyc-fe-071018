3e904179d51d36546751fdd5b212b1a52914cf35
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-08 00:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
-document.body.remove()
+body.remove()
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
