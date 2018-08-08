e575415d8c3e0a65781cd04428428d0d8f61af81
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-08 00:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ document.getElementById('main').remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
-h1.id = 'victory'
+newHeader.id = 'victory'
